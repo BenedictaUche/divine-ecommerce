96f4ce3698ff556032a0d3c23012175e9da6bdc9
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-12-10 03:55:04 +0100

    added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between  ${inter.className}`}
     >
-      <h1>Title</h1>
+      <h1>homepage</h1>
     </main>
   )
 }
